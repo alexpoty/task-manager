d812434d25d5f20c92eef5e4cf29ba3fd6e38c81
alexpoty <alexpotyagaylo@gmailc.om>
2024-12-18 22:19:01 +0200

    Added Junit in pom.xml Modified TaskDto

--- src/main/java/com/alexpoty/TaskManagmentSystem/dto/TaskDto.java
+++ src/main/java/com/alexpoty/TaskManagmentSystem/dto/TaskDto.java
@@ -2,11 +2,13 @@ package com.alexpoty.TaskManagmentSystem.dto;
 
 import com.alexpoty.TaskManagmentSystem.util.Priority;
 import com.alexpoty.TaskManagmentSystem.util.Status;
+import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDate;
 
 @Data
+@Builder
 public class TaskDto {
 
     private String title;
